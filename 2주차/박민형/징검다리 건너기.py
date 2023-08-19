529ddcd49d2d0038496493ecffb89b864d943224
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-08-19 17:44:14 +0900

    dp

--- 2주차/박민형/징검다리 건너기.py
+++ 2주차/박민형/징검다리 건너기.py
@@ -33,62 +33,6 @@ for i in range(3, N):
         e, one_jump, two_jump = one_jump, two_jump, 1e9
     _min = min(_min, e)
 
-print(_min)import sys
-input = sys.stdin.readline
-
-N = int(input())
-stone = []
-
-# dp 배열 생성
-dp = [1e9]*N
-dp[0] = 0
-for i in range(N-1):
-    s, b = map(int, input().split())
-    stone.append((s, b))
-    if i+1<N: dp[i+1] = min(dp[i+1], dp[i]+s)
-    if i+2<N: dp[i+2] = min(dp[i+2], dp[i]+b)
-
-# 매우 큰 점프 적용해보며 최솟값 찾기
-K = int(input())
-
-_min = dp[-1]
-for i in range(3, N):
-    e, one_jump, two_jump = dp[i-3]+K, 1e9, 1e9
-    for j in range(i, N-1):
-        if i+1 < N: one_jump = min(one_jump, e+stone[j][0])
-        if i+2 < N: two_jump = min(two_jump, e+stone[j][1])
-        # print(one_jump, two_jump)
-        e, one_jump, two_jump = one_jump, two_jump, 1e9
-    _min = min(_min, e)
-
-print(_min)import sys
-input = sys.stdin.readline
-
-N = int(input())
-stone = []
-
-# dp 배열 생성
-dp = [1e9]*N
-dp[0] = 0
-for i in range(N-1):
-    s, b = map(int, input().split())
-    stone.append((s, b))
-    if i+1<N: dp[i+1] = min(dp[i+1], dp[i]+s)
-    if i+2<N: dp[i+2] = min(dp[i+2], dp[i]+b)
-
-# 매우 큰 점프 적용해보며 최솟값 찾기
-K = int(input())
-
-_min = dp[-1]
-for i in range(3, N):
-    e, one_jump, two_jump = dp[i-3]+K, 1e9, 1e9
-    for j in range(i, N-1):
-        if i+1 < N: one_jump = min(one_jump, e+stone[j][0])
-        if i+2 < N: two_jump = min(two_jump, e+stone[j][1])
-        # print(one_jump, two_jump)
-        e, one_jump, two_jump = one_jump, two_jump, 1e9
-    _min = min(_min, e)
-
 print(_min)
 
 # 틀린 코드

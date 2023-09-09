ebcaa04bc72de921e4c0505b6fff7b103c581a0e
안녕하세요 <khl1154@naver.com>
2023-09-09 19:15:41 +0900

    Update 순위.java

--- [2023.09.04 - 2023.09.10] - 1주차/부끄러운어피치/순위.java
+++ [2023.09.04 - 2023.09.10] - 1주차/부끄러운어피치/순위.java
@@ -2,7 +2,7 @@ public class 순위 {
     public int solution(int n, int[][] results) {
         int answer = 0;
 
-        /* 승리 전적표 채우기 */
+        //전적대를 구할 맵을 만든다.
         boolean[][] map = new boolean[n + 1][n + 1];
         for (int[] i : results)
             map[i[0]][i[1]] = true;
@@ -13,7 +13,7 @@ public class 순위 {
 
 
 
-        /* 플루이드 와샬 알고리즘을 통해 승리 채우기 */
+        /* 플루이드 와샬 알고리즘  승리 채우기 */
         for (int k = 1; k <= n; ++k) { // 가운데 선수
             for (int i = 1; i <= n; ++i) { // 강한 선수
                 for (int j = 1; j <= n; ++j) { // 약한 선수

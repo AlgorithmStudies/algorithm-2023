ea32cd6003b83787c5e00a41a885d17dc5011445
안녕하세요 <khl1154@naver.com>
2023-09-24 16:08:04 +0900

    Update 섬연결하기.java

--- [2023.09.25 - 2023.10.01] - 4주차/부끄러운어피치/섬연결하기.java
+++ [2023.09.25 - 2023.10.01] - 4주차/부끄러운어피치/섬연결하기.java
@@ -1,9 +1,4 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
-class 섬연결하기 {
-    private int[] findParent;	//부모배열
-    public int solution(int n, int[][] costs) {
+import java.util.*;
 /*
         가중치를 기준으로 배열을 정렬
         그렇게 정렬된 배열을 이용을 해서 가중치가 가장 작은 것 부터 연결
@@ -11,43 +6,51 @@ class 섬연결하기 {
         보통 왼쪽의 값이 작으므로 왼쪽의 값을 부모노드로 사용
         모든 costs를 확인해서 결과값을 찾기
 */
-        //가중치를 기준으로 오름차순 정렬
-        Arrays.sort(costs, new Comparator<int[]>() {
-
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[2] == o2[2]) {
-                    return o1[1] - o2[1];
-                } else {
-                    return o1[2] - o2[2];
-                }
-            }
-        });
+class 섬연결하기 {
+    public int solution(int n, int[][] costs) {
+        Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2])); // 가중치 기준 정렬
 
         //부모노드 초기화 , 시작은 자기자신이 부모
-        findParent = new int[n];
-        for (int i = 0; i < findParent.length; i++) {
-            findParent[i] = i;
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i; // 각 섬을 개별 집합으로 초기화
         }
 
-        int answer = 0;
-        for (int i = 0; i < costs.length; i++) {	//출발지를 start, 도착지를 end
-            int start = find(costs[i][0]);
-            int end = find(costs[i][1]);
-            if (start != end) { // 부모가 같지 않다면 연결이 안된 최솟값이므로
-                findParent[end] = start; // 연결
-                answer += costs[i][2];
+        int minCost = 0;
+        int edgeCount = 0;//섬의 연결여부 판단.
+
+        for (int[] cost : costs) {
+            int from = cost[0];
+            int to = cost[1];
+            int costValue = cost[2];
+            // 부모가 같지 않다면 연결이 안된 최솟값이므로
+            if (find(parent, from) != find(parent, to)) {
+                // 두 섬이 같은 집합에 속하지 않으면 연결
+                union(parent, from, to);
+                minCost += costValue;
+                edgeCount++;
+            }
+
+            if (edgeCount == n - 1) {
+                // 모든 섬이 연결되면 종료
+                break;
             }
         }
-        return answer;
 
+        return minCost;
     }
-    //부모노드 값을 찾는 메서드
-    private int find(int child) {
-        if (findParent[child] == child) {
-            return child;
-        } else {
-            return findParent[child] = find(findParent[child]);
+
+    private int find(int[] parent, int node) {
+        if (parent[node] == node) {
+            return node;
         }
+        parent[node] = find(parent, parent[node]);
+        return parent[node];
+    }
+
+    private void union(int[] parent, int a, int b) {
+        int rootA = find(parent, a);
+        int rootB = find(parent, b);
+        parent[rootA] = rootB;
     }
 }
